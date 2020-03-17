911cbfdff1d8e597b0dcc19a6158e0645841e34e
Bar Horing Amir <bar.apple@gmail.com>
2020-03-17 13:27:21 +0200

    save

--- src/QuesionsContainer.js
+++ src/QuesionsContainer.js
@@ -1,31 +1,10 @@
 import React from "react";
+import "./Questions.css";
 
 function QuesionsContainer() {
   return (
-    <button
-      style={{
-        width: "190px",
-        height: "55px",
-        // color: "#FAFAFA",
-        opacity: "100%",
-        radius: "23px",
-        border: "5px solid red",
-        background: "#FAFAFA",
-        border: "2px solid #D9E6EB",
-        borderRadius: "23px"
-      }}
-    >
-      <div
-        style={{
-          fontFamily: "OpenSans-Semibold",
-          fontSize: "28px",
-          color: "#496472",
-          letterSpacing: 0,
-          textAlign: "center"
-        }}
-      >
-        11
-      </div>
+    <button className="Rectangle">
+      <div className="ReactangleSmall OptionText">11</div>
     </button>
   );
 }
