d76c28b65f6fceb8d07a5e5e3074107e3d12029f
Bar Horing Amir <bar.apple@gmail.com>
2020-03-19 19:47:21 +0200

    added credit

--- src/Credit.js
+++ src/Credit.js
@@ -1,7 +1,24 @@
 import React from "react";
+// import * from "./github.svg";
+import logo from "./logo.svg";
+
+import github from "./github.svg";
+import stackoverflow from "./stackoverflow.svg";
 
 const Credit = () => {
-  return <div>Created by Bar Horing Amir </div>;
+  return (
+    <div>
+      <div style={{ fontSize: "17px" }}>Created by Bar Horing Amir</div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <a href="https://stackoverflow.com/users/6683217/bar-horing">
+          <img src={stackoverflow} className="creditIcon" alt="stackoverflow" />
+        </a>
+        <a href="https://github.com/barhoring">
+          <img src={github} className="creditIcon" alt="github" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Credit;
