9edcaabe5c273b8d61bdac18cc9a2995fec5e989
Bar Horing Amir <bar.apple@gmail.com>
2020-03-19 19:13:55 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Questions from "./Questions";
-import QuesionsContainer from "./QuesionsContainer";
-import logo from "./logo.svg";
+import Credit from "./Credit";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Questions />
+      <Credit />
     </div>
   );
 }
